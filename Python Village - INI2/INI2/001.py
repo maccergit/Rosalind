a4569196682b3dbb40720c111651a22c686e74f2
johnmcalister <johnmcalister@c02f615hmd6w.lan>
2023-06-14 19:30:16 -0600

    Update to python3 syntax, add personal observations

--- Python Village - INI2/INI2/001.py
+++ Python Village - INI2/INI2/001.py
@@ -16,10 +16,16 @@ Sample Output
 def processData(inFileName):
     with open(inFileName) as datafile:
         return sum(int(x.strip()) ** 2 for x in datafile.readline().split(" "))
+
+"""
+Personal observations :
+- Processing files, strings, etc... as sequences is very common in Python - here, we read the only line, and split it into pieces, stripping the whitespace and converting
+the strings to int values.  Once we have a sequence of int, we then square the values and sum them up - and that's the answer.
+"""
     
 assert processData('sample.txt') == 34
 
 with open('results.txt', 'w') as resultsfile:
     result = processData('rosalind_ini2_1_dataset.txt')
-    print result
+    print(result)
     resultsfile.write(str(result))
