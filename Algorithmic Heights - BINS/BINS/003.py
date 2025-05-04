b16a5cbb2a44230a0799f80c89239e1bae3e263e
johnmcalister <johnmcalister@c02f615hmd6w.lan>
2025-05-03 22:54:41 -0600

    working binary search

--- Algorithmic Heights - BINS/BINS/003.py
+++ Algorithmic Heights - BINS/BINS/003.py
@@ -15,30 +15,91 @@ Sample Dataset
 Sample Output
  4 1 -1 -1 4 2
 
-Brute force linear lookup - NOT binary search - use to verify other methods
-Binary search iteration 1 - copy of working linear search
+Binary search iteration 2 - add simple cases that will be exit conditions of recursion
 """
 
 def readlineValues(infile):
     return [x.strip() for x in infile.readline().split()]
 
 def lookup(dataSorted, value):
-    for x in range(1, len(dataSorted) + 1):
-        if dataSorted[x - 1] == value:
-            return x
-    return -1
+    # Exit conditions
+    if len(dataSorted) == 0:
+        return -1
+    if len(dataSorted) == 1:
+        if value == dataSorted[0]:
+            return 0
+        else:
+            return -1
+    
+    # Split the array into 2 arrays
+    midIndex = len(dataSorted) // 2
+    leftSorted = dataSorted[:midIndex]
+    rightSorted = dataSorted[midIndex:]
+    retval = None
+    
+    # Determine which subarray to search
+    if value < rightSorted[0]:
+        # no index adjustment needed
+        retval =  lookup(leftSorted, value)
+    elif value == rightSorted[0] and leftSorted[len(leftSorted) - 1] == rightSorted[0]:
+        retval = lookup(leftSorted, value)
+    else:
+        retval = lookup(rightSorted, value)
+        if not(retval < 0):
+            retval += midIndex
+            
+    return retval
+
+def processParsedData(dataSorted, dataUnsorted):
+    return ' '.join(str(val if (val := lookup(dataSorted, x)) < 0 else val + 1) for x in dataUnsorted)
 
 def processData(inFileName):
     with open(inFileName) as datafile:
         # With Python, we don't care about the size of the data arrays, so we just skip them
         datafile.readline()
         datafile.readline()
-        dataSorted = readlineValues(datafile)
-        dataUnsorted = readlineValues(datafile)
-    return ' '.join(str(lookup(dataSorted, x)) for x in dataUnsorted)
+        dataSorted = [int(x) for x in readlineValues(datafile)]
+        dataUnsorted = [int(x) for x in readlineValues(datafile)]
+    return processParsedData(dataSorted, dataUnsorted)
+
+assert lookup([], 40) == -1
+assert lookup([], 10) == -1
+assert lookup([], 35) == -1
+assert lookup([], 15) == -1
+assert lookup([], 20) == -1
+
+assert lookup([10], 40) == -1
+assert lookup([20], 40) == -1
+assert lookup([30], 40) == -1
+assert lookup([40], 40) == 0
+assert lookup([50], 40) == -1
+assert lookup([10], 10) == 0
+assert lookup([10], 20) == -1
     
 assert processData('sample.txt') == '4 1 -1 -1 4 2'
+
+assert lookup([10, 20, 30, 40, 50, 60, 70, 80, 90], 85) == -1
+assert lookup([10, 20, 30, 40, 50, 60, 70, 80, 90], 80) == 7
+assert lookup([10, 20, 30, 40, 50, 60, 70, 80, 90], 10) == 0
+assert lookup([10, 20, 30, 40, 50, 60, 70, 80, 90], 20) == 1
+assert lookup([10, 20, 30, 40, 50, 60, 70, 80, 90], 30) == 2
+assert lookup([10, 20, 30, 40, 50, 60, 70, 80, 90], 40) == 3
+assert lookup([10, 20, 30, 40, 50, 60, 70, 80, 90], 50) == 4
+assert lookup([10, 20, 30, 40, 50, 60, 70, 80, 90], 60) == 5
+assert lookup([10, 20, 30, 40, 50, 60, 70, 80, 90], 70) == 6
+assert lookup([10, 20, 30, 40, 50, 60, 70, 80, 90], 90) == 8
+
+assert processParsedData([10, 20, 30, 40, 50, 60, 70, 80, 90], [85, 80, 10, 20, 30, 40, 50, 60, 70, 90]) == '-1 8 1 2 3 4 5 6 7 9'
 
 with open('results_binary.txt', 'w') as resultsfile:
     result = processData('rosalind_bins_1_dataset.txt')
     resultsfile.write(str(result))
+    
+# TODO - wiki space needs to note the following : data needs to be converted to ints, because sring compare of ints is incorrect (show examples).
+# TODO - the 1-based indexing of Rosalind does not match the 0-based indexing of Python - easy approach is to do everything in 0-based indexing, and then
+# convert to 1-based indexing once we have the results.  Problem is this is easy to forget.
+# TODO - problem statement and sample data do not show any special requirements for handling duplicate data.  Binary search algorithm typically returns first found
+# index that satisfies the search - NOT the smallest index that satisfies the search (this makes it a tiny bit faster).  The accepted solution only works if we
+# assume it wants the smallest index that satisfies the binary search (which is what a linear search provides).
+# TODO - use "bisect_left" to do the binary search using a library
+# TODO - use hash lookup - does not care if indexed data is sorted, and can be used when passed unsorted data - binary search would require a sort step.
